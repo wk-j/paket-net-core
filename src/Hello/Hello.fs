02cbe6d33d1efc4c701714a5ff9322ce1b367f4a
wk-j <somnuk.wk@gmail.com>
2017-12-05 21:37:20 +0700

    Added fields

--- src/Hello/Hello.fs
+++ src/Hello/Hello.fs
@@ -2,9 +2,11 @@ module Hello
 
 open FSharp.Data
 
-type A = JsonProvider<"{}">
+type A = JsonProvider<"""{ "a": 100, "b": 200 }""">
 
 [<EntryPoint>]
 let main argv =
-    A.Parse("{}") |> printfn "%A"
+    let rs = A.Parse("""{ "a": 300, "b": 600 }""") 
+    rs.A |> printfn "%A"
+    rs.B |> printfn "%A"
     0 // return an integer exit code
